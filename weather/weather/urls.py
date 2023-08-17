b52c39177c112af3b2d9405692ce259244511d3d
rainbowdwarf <glubokov3099@gmail.com>
2023-08-17 14:18:40 +0300

    add urls

--- weather/weather/urls.py
+++ weather/weather/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = 'weather_app'
 urlpatterns = [
@@ -23,3 +25,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("__debug__/", include("debug_toolbar.urls")),
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
